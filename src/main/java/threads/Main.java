772379c8aebda2415efcf3df6aa8aeec6215c8a8
Saba <121631242+SabaEvgenidze@users.noreply.github.com>
2023-01-01 23:07:07 +0400

    Initial commit

--- src/main/java/threads/Main.java
+++ src/main/java/threads/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] argv){
         Runnable r = new Task();
         Thread  task = new Thread(r);
-        r.run();
+        task.start();
 
         for (int doc=1; doc<=10; doc++){
             System.out.println("^^ Doc #: " + doc);
